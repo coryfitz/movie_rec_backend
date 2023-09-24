04363d9735f9c04bab500327e7661593b705e252
Cory Fitz <coryalanfitz@gmail.com>
2023-09-24 20:31:15 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -14,15 +14,12 @@ from datetime import timedelta
 from pathlib import Path
 import os
 from environs import Env
-
-
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
